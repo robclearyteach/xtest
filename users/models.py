b6d46faf96a80d1ae76ea360f03b308d9c9effde
robteach <robert.cleary.teach@gmail.com>
2026-05-12 09:24:08 +0100

    updated models.py save() to only image.path if an image and simplify if user.profile.image in profile.html

--- users/models.py
+++ users/models.py
@@ -10,13 +10,26 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
-    
+
+    #updated save() to only Image.open(self.image.path) 
+    #   if an image was provided (otherwise causes exception)
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+        if self.image and self.image.path:
+            img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.image.path)    
+                
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
+
+    #     img = Image.open(self.image.path)
+
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
